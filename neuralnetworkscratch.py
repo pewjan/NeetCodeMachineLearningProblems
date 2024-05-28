240655e86b98ce2c49f65263570304e99b89ae74
Pujan <62914735+pewjan@users.noreply.github.com>
2024-05-27 21:45:13 -0500

    Update neuralnetworkscratch.py

--- neuralnetworkscratch.py
+++ neuralnetworkscratch.py
@@ -7,10 +7,12 @@ import tensorflow as tf
 
 class Layer:
     def __init__(self, num_inputs, num_neurons):
+                                        # each row is different weight and each column is different neuron
         self.weights = np.random.randn(num_inputs, num_neurons) * .01;
         self.biases = np.random.randn((1, num_neurons));
     def forward(self, input):
         self.inputs = input;
+                                        #each feature will be multiplied to each weight then added
         self.output = self.sigmoid(np.dot(self.inputs, self.weights)+self.biases);
         return self.output;
     def sigmoid(self, z):
@@ -19,4 +21,14 @@ class Layer:
         return z*(1-z);
 
 class NeuralNetwork:
-    pass
+    #layers in the NN
+    def __init__(self):
+        self.layers = [];
+    def addLayer(self, layer):
+        self.layers.append(layer);
+    #forward propigation and we send each layer the new input by saving the output
+    def forward(self, X):
+        for layer in self.layers:
+            X = layer.forward(X);
+        return X;
+        
