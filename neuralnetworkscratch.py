f2d0d1bc80f31f52ad2f020c01a32685d1e72de1
Pujan <62914735+pewjan@users.noreply.github.com>
2024-05-29 20:22:36 -0500

    added gradient descent with backward

--- neuralnetworkscratch.py
+++ neuralnetworkscratch.py
@@ -31,4 +31,36 @@ class NeuralNetwork:
         for layer in self.layers:
             X = layer.forward(X);
         return X;
+    def backward(self, X, y, learning_rate):
         
+        #loss
+        output = self.layers[-1].output;
+        loss = y - output;
+        
+        for i in range(reversed(len(self.layers))):
+            layer = self.layers[i];
+            if i == len(self.layers)-1:
+
+                #dL/dz = loss * Q'(z) 
+                dL = loss * layer.sigmoid_der(layer.output)
+            else:
+                nextLayer = self.layers[i+1];
+                dL = np.dot(dL, nextLayer.weights.T) * layer.sigmoid_der(layer.output)
+            
+            dL_dw = np.dot(layer.input.T, dL); 
+            dL_db = np.sum(dL, axis=0, keepdims=True)
+                
+            # Gradient descent step: update weights and biases
+            layer.weights += learning_rate * dL_dw
+            layer.biases += learning_rate * dL_db
+    def train(self, X, y, epochs, learning_rate):
+        for epoch in range(epochs):
+            self.forward(X)  # Forward pass
+            self.backward(X, y, learning_rate)  # Backward pass
+            if epoch % 100 == 0:
+                output = self.forward(X)
+                loss = np.mean((y - output) ** 2)
+                print(f'Epoch {epoch}, Loss: {loss}')
+
+    def predict(self, X):
+        return self.forward(X)
